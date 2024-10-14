24d4f20a233132e5d741d897c04ee7f7bd5b439e
imnotrafa <abreurafa22@gmail.com>
2024-10-14 16:13:29 -0400

    fixed warning errors

--- src/Pages/Sponsors.js
+++ src/Pages/Sponsors.js
@@ -1,15 +1,13 @@
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
 import "./sponsors_style.css";
 import audibleLogo from "../resources/sponsorsImages/audibleLogo.jpg"
 const SponsorsPage = () =>{
-    const sponsorsList = ["amazon","audible","google"]
     return (
         <>
         <Container className="mainContainer">
             <h1>A special thank you to our 24’-25’ Sponsors</h1>
             <Container className="sponsorsContainer">
-               <img src={audibleLogo}></img>
+               <img src={audibleLogo} alt=""></img>
             </Container>
         </Container>
         
