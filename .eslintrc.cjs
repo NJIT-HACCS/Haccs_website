99ce0c0b7aaedcd970dbd503637da4ad9c652656
Emersonp7 <emersonp935@gmail.com>
2025-08-10 01:09:06 -0400

    Set up for v2 website

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
+  settings: { react: { version: "detect" } },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "react/react-in-jsx-scope": "off"
+  },
+  ignorePatterns: ["build/", "node_modules/"]
+};
