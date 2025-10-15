396be1b96eb444307a23ad3addd31b5d103e19ab
Emerson Perez <emersonp935@gmail.com>
2025-10-15 02:02:03 -0400

    [Changes] 	* Finished Achievements Page 	* Slight Change to Footer Components 	* Added Links to Icons for Footer.
    [Test]
    	* npm start
    	* npm run build

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,33 @@
 import { FaDiscord, FaInstagram, FaLinkedin } from "react-icons/fa6";
-import { Stack, Icon, Box, HStack } from '@chakra-ui/react'
+import { Stack, Icon, Box, HStack, Link } from '@chakra-ui/react'
 import React, { ElementType } from "react";
 
 interface Footerprops {
     direction: string,
 };
 
+interface LinkedIconsProps {
+	redirect: string,
+	icon: ElementType,
+}
+
+const LinkedIcon = (props: LinkedIconsProps ) : React.JSX.Element => {
+	return (
+		<Link href={props.redirect}>
+			<Icon as={props.icon} />
+		</Link>
+	)
+}
 const verticalFooter: React.JSX.Element = 
 (   
     <Stack style={{
         marginLeft: '5%', marginTop: '3%', gap: '15px'
     }}>
-        <Icon as={FaDiscord as ElementType} />
-        <Icon as={FaInstagram as ElementType} />
-        <Icon as={FaLinkedin as ElementType} />
-        <Box style={{border: '1px solid white', height: '70px', width: '0px', marginLeft: '50%' }} />
+	<LinkedIcon redirect="https://discord.gg/35uBvfY5Dr" icon={FaDiscord as ElementType} />
+	<LinkedIcon redirect="https://www.instagram.com/njit_haccs/" icon={FaInstagram as ElementType} />
+	<LinkedIcon redirect="https://www.linkedin.com/in/njit-haccs-679151296/" icon={FaLinkedin as ElementType} />
+
+	<Box style={{border: '1px solid white', height: '70px', width: '0px', marginLeft: '50%' }} />
     </Stack>
 );
 
@@ -23,11 +36,11 @@ const horizontalFooter: React.JSX.Element =
     <HStack style={{
         marginLeft: '5%', marginTop: '3%', gap: '15px'
     }}>
-        <Box style={{border: '1px solid white', height: '0px', width: '47px' }} />
-        <Icon as={FaDiscord as ElementType} />
-        <Icon as={FaInstagram as ElementType} />
-        <Icon as={FaLinkedin as ElementType} />
+    	<Box style={{border: '1px solid white', height: '0px', width: '47px' }} />
 
+		<LinkedIcon redirect="https://www.linkedin.com/in/njit-haccs-679151296/" icon={FaLinkedin as ElementType} />
+		<LinkedIcon redirect="https://www.instagram.com/njit_haccs/" icon={FaInstagram as ElementType} />
+		<LinkedIcon redirect="https://discord.gg/35uBvfY5Dr" icon={FaDiscord as ElementType} />
     </HStack>
 
 );
